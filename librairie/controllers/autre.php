93ddd317975b51f53f5d6c24ab790f0e78a92ddb
Nicolas Baptiste <nicolas.baptiste@gmail.com>
2012-04-25 18:10:59 +0200

    Modification de controleur

--- librairie/controllers/autre.php
+++ librairie/controllers/autre.php
@@ -1,9 +1,11 @@
 <?php
+/**
+ * Controleur de test
+ *
+ * @author Nicolas Baptiste
+ */
 Class Autre extends Controller{
 	public function index() {
-		//$trace = debug_backtrace();
-		//var_dump($trace);
-		//krsort($trace);
 		$view = Load::view('home',$data);
 		$this->afficherVue($view);
 	}
